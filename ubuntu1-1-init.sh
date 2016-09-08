ce425a7569da52a4a793c3581377b8601b254d9f
Chalermrat Srinara <chalermrat@Chalermrats-MacBook-Pro.local>
2016-09-09 01:19:08 +0700

    fix shell init

--- ubuntu1-1-init.sh
+++ ubuntu1-1-init.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Checkout code
-git clone https://github.com/karrung/cicd-lab.git
-cd cicd-lab
-
 # Make directory prepare for docker volume assets
 mkdir -p ./assets/gitlab
 mkdir -p ./assets/jenkins_home
@@ -11,5 +7,5 @@ mkdir -p ./assets/postgresql
 mkdir -p ./assets/redis
 
 # Set docker insecure-registry pull/push mode
-sudo echo 'DOCKER_OPTS="--insecure-registry 192.168.1.200:5000"'
-sudo service docker restart
+echo 'DOCKER_OPTS="--insecure-registry 192.168.1.200:5000"' | tee -a /etc/default/docker
+service docker restart
