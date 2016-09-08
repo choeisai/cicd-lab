29952704685703e8a6c31a81fdfe1f7bad9500ce
Chalermrat Srinara <chalermrat@Chalermrats-MacBook-Pro.local>
2016-09-09 01:53:12 +0700

    docker compose for ubuntu 1

--- ubuntu1-1-init.sh
+++ ubuntu1-1-init.sh
@@ -5,6 +5,7 @@ mkdir -p ./assets/gitlab
 mkdir -p ./assets/jenkins_home
 mkdir -p ./assets/postgresql
 mkdir -p ./assets/redis
+chmod 777 ./assets/*
 
 # Set docker insecure-registry pull/push mode
 echo 'DOCKER_OPTS="--insecure-registry 192.168.1.200:5000"' | tee -a /etc/default/docker
